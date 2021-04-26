69fb7484feef2045792c714ff3cfb4ca09f253a8
Laith Alwani <laithalwani@gmail.com>
2021-04-26 00:39:24 -0400

    added build file

---
+++ client/build/precache-manifest.160fb556a410394bfdecb22898ead9c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52143cb850cc39c56504",
+    "url": "/static/css/main.dcc39abd.chunk.css"
+  },
+  {
+    "revision": "52143cb850cc39c56504",
+    "url": "/static/js/main.afcc3cc8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "2a645c866fec88e20140",
+    "url": "/static/js/2.70e62dbc.chunk.js"
+  },
+  {
+    "revision": "b8b4a64ff0d229c83f21d293d9b9095a",
+    "url": "/index.html"
+  }
+];
